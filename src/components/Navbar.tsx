b70f36190b5ea146cb5b5914de47e40aa8e3c2cc
Natalia1705 <lau.nat.her.her@gmail.com>
2024-07-17 16:15:07 -0600

    add menu component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,17 @@
+import Link from "next/link"
+import Menu from "./Menu"
+
 const Navbar = () => {
     return (
-    <div className=''>Navbar</div>
+    <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
+        <div className="h-full flex items-center justify-between ">
+        {/*Mobile*/}
+        <Link href="/">
+            <div className="text-2xl tracking-wide">Covita</div>
+        </Link>
+        <Menu />
+        </div>
+    </div>
     )
 }
 
